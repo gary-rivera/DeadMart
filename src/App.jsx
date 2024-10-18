f03b1499a7ab8610aea60a5656c189ced26af95d
gary-rivera <a.gary.rivera@gmail.com>
2024-10-18 10:13:01 -0700

    base setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import './App.css';
+import BaseItemContainer from './components/BaseItemContainer';
+import {ALL_ITEMS} from 'deadlock-content';
+
 
 function App() {
-	return <></>;
+	// console.log('items', ALL_ITEMS)
+	// const { weaponItems, vitalityItems, spiritItems } = ALL_ITEMS.reduce((acc, curr) => {
+	// 	const key = `${curr.type}Items`
+
+	// 	if (acc[key])
+	// 		acc[key].push(curr)
+
+	// 	return acc
+	// }, { weaponItems: [], vitalityItems: [], spiritItems: [] })
+
+	// console.log('items acc', {
+	// 	weaponItems, vitalityItems, spiritItems
+	// })
+	return (
+		<>
+
+			<BaseItemContainer/>
+			{/* <BaseItemContainer/>
+			<BaseItemContainer/>  */}
+
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/components/BaseItemContainer.jsx
+++ src/components/BaseItemContainer.jsx
@@ -1,5 +1,25 @@
+import styles from '../styles/BaseItemContainer.module.css'
+
+// TODO: create tier sections
+// TODO: split passed down items into tiers
+// TODO: render items to respective tier sections
 function BaseItemContainer() {
-	return;
+	return(
+		<div className={styles.BaseItemContainer}>
+			<div className={`${styles.tierOptionA} ${styles.tierRow}`}>
+				<div className={styles.sidebar}>tier 1</div>
+			</div>
+			<div className={`${styles.tierOptionB} ${styles.tierRow}`}>
+				<div>tier 2</div>
+			</div>
+			<div className={`${styles.tierOptionA} ${styles.tierRow}`}>
+				<div>tier 3</div>
+			</div>
+			<div className={`${styles.tierOptionB} ${styles.tierRow}`}>
+				<div>tier 4</div>
+			</div>
+		</div>
+	)
 }
 
-export default BaseItemContainer;
+export default BaseItemContainer
